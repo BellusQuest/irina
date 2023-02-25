de51d125d19216fc0078d88c7622489fe9de53b7
Shindou Mihou <git@mihou.pw>
2023-02-26 02:32:48 +0800

    change(loading): fix mobile loading

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -11,18 +11,19 @@
         }, 1000)
         setTimeout(() => {
             showPage = true
-            document.getElementById('main').classList.replace('blur', 'blurTransition')
+            const main = document.getElementById('main')
+            main.classList.replace('hidden', 'flex')
+            main.classList.replace('md:blur', 'blurTransition')
         }, 1500)
     })
 </script>
-
 <div id="container" class="p-10 sofia w-full flex flex-col">
-    <div class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-        {#if !showPage}
+    {#if !showPage}
+        <div class="h-screen m-auto align-middle justify-center flex flex-row items-center md:min-h-fit md:m-0 md:absolute md:top-1/2 md:left-1/2 md:transform md:-translate-x-1/2 md:-translate-y-1/2">
             <img id="loader" src="/bellus.min.png" class="w-16 animate-bounce" alt="Loading"/>
-        {/if}
-    </div>
-    <main id="main" class="w-full flex flex-grow min-h-screen mx-auto my-auto max-w-6xl xl:px-12 xl:py-6 blur">
+        </div>
+    {/if}
+    <main id="main" class="w-full flex-grow min-h-screen m-auto max-w-6xl xl:px-12 xl:py-6 hidden md:flex md:blur">
         <slot />
     </main>
 </div>
